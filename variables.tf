bbe50b8501e6ed059883172f304205aed1585d27
jonathan <joanthan@jonathan.co.il>
2023-09-02 18:19:32 +0300

    WIP

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "vpc_id" {
-	type = string
+  type = string
 }
 
 variable "my_ip_with_cidr" {
-	type = string
-	description = "Provide your IP eg. 104.194.51.113/32"
+  type        = string
+  description = "Provide your IP eg. 104.194.51.113/32"
 }
 
 variable "public_key" {
-	type = string
+  type = string
 }
 
 variable "instance_type" {
-	type = string
-	default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "server_name" {
-	type = string
-	default = "Apache Example Server"
+  type    = string
+  default = "Apache Example Server"
 }
